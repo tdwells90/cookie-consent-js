b624609072a18c28f7989d24b2a7bf7fd2dde81e
Tom <tdwells90@gmail.com>
2018-11-11 21:32:37 +0000

    Re-organise file structure and include scripts in main class

--- cookie-consent-js.php
+++ cookie-consent-js.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Simple Cookie Consent JS
-Plugin URI:  https://github.com/simonrcodrington/Introduction-to-WordPress-Plugins---Location-Plugin
+Plugin URI:  https://github.com/tdwells90/cookie-consent-js
 Description: Simple cookie consent JS for Google Tag Manager.
 Version:     0.1.0
 Author:      Nfty Creative
@@ -12,6 +12,7 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 defined('ABSPATH') or die('Nope, not accessing this');
 
+// Main plugin class
 if (!class_exists('cookie_consent_js')) {
 
     class cookie_consent_js
@@ -19,9 +20,9 @@ if (!class_exists('cookie_consent_js')) {
 
         public static function register()
         {
-
+            //include scripts
+            include(plugin_dir_path(__FILE__) . 'inc/include-scripts.php');
         }
-
     }
 
     cookie_consent_js::register();
