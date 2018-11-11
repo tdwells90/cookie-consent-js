d86469486c549737dcc7918ec02fe580e83f6a8a
Tom <tdwells90@gmail.com>
2018-11-11 19:54:26 +0000

    Cookie Consent Plugin Init

---
+++ cookie-consent-js.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Simple Cookie Consent JS
+Plugin URI:  https://github.com/simonrcodrington/Introduction-to-WordPress-Plugins---Location-Plugin
+Description: Creates an interfaces to manage store / business locations on your website. Useful for showing location based information quickly. Includes both a widget and shortcode for ease of use.
+Version:     1.0.0
+Author:      Simon Codrington
+Author URI:  http://www.simoncodrington.com.au
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+defined('ABSPATH') or die('Nope, not accessing this');
