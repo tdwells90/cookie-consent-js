03ea1126d1a85785ea95da0c09ea589714c3ec5d
Tom <tdwells90@gmail.com>
2018-11-15 17:40:27 +0000

    Minor CSS updates

--- cookie-plugin.js
+++ cookie-plugin.js
@@ -187,8 +187,8 @@ var cookieConsent = (function($) {
 				container: $("#cookie-consent"),
 				elements: {
 					header: "",
-					message: `<div class="cookie-heading"><h2>${header}</h2></div><div class="cookie-consent" id="cookieconsent:desc">${mainText}</div>`,
-					messagelink: `<div class="cookie-heading"><h2>${header}</h2></div><div class="cookie-consent" id="cookieconsent:desc">${mainText} <a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="${
+					message: `<div class="cookie-consent" id="cookieconsent:desc"><h2>${header}</h2>${mainText}</div>`,
+					messagelink: `<div class="cookie-consent" id="cookieconsent:desc"><h2>${header}</h2>${mainText} <a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="${
 						options.url
 					}" target="_blank">Find out more</a></div><div class="cookie-settings"><form><div class="settings-heading"><h2>Cookie Settings</h2><a tabindex="0" class="${primaryButton} settings-dismiss">Save settings</a></div><div class="settings-form"><h3>${essentialHeader}</h3><p>${essentialText}</p>${cookieText}<div class="settings-indiv">${tracking}${marketing}</div></div></form></div>`,
 					dismiss: `<a aria-label="dismiss cookie message" tabindex="0" class="${primaryButton} cc-btn cc-dismiss">Accept</a>${showSettings}`,
